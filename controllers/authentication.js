0a6e858450da7ed7dedd841d4a631d4bad97cf5e
Madeleine Huish <madeleinehuish@gmail.com>
2019-01-05 16:43:18 -0800

    figured it out lol...

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -26,11 +26,6 @@ exports.signup = function(req, res, next) {
   const lastname = req.body.lastname;
   const email = req.body.email;
   const password = req.body.password;
-  console.log('firstname: ', req.body.firstname);
-  console.log('lastname: ', req.body.lastname);
-  console.log('email: ', req.body.email);
-  console.log('PORT in production:::: ', process.env.PORT);
-  console.log('process.env.NODE_ENV::: ', process.env.NODE_ENV);
 
   if (!email || !password) {
     return res.status(422).send({ error: 'You must provide email and password'});
