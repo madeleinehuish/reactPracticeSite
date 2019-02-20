c08b6f24ff6d07cbd86d5b5a0b9b4517527afd48
Madeleine Huish <madeleinehuish@gmail.com>
2019-02-19 20:56:53 -0800

    ...

--- client/src/reducers/cards.js
+++ client/src/reducers/cards.js
@@ -8,6 +8,7 @@ export default function(state=INITIAL_STATE, action) {
 		case FETCH_CARDS:
 			return { ...state, base: action.payload, cards: action.payload };
 			break;
+
 		// case MAGIC_ERROR:
 		// 	return {...state, errorMessage: action.payload }
 		case UPDATE_CARDS:
